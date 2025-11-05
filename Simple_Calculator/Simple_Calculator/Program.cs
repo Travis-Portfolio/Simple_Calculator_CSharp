294ae175309e7e7c99685a5eada7f66a60fbe822
deboartm <deboartm@miamioh.edu>
2025-11-05 17:29:46 -0500

    Add, Subtract, Multiply
    Implementing functionality for addition, subtraction and multiplication of 2 integers.

--- Simple_Calculator/Simple_Calculator/Program.cs
+++ Simple_Calculator/Simple_Calculator/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Subtract(1, 2));
+            Console.WriteLine(Multiply(1, 2));
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        static int Multiply(int x, int y)
+        {
+            return x * y;
         }
     }
 }
