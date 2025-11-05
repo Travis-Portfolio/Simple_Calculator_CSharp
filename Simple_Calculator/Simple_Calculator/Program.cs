07332817af14e1761b288d4d4da3020b69d105e1
deboartm <deboartm@miamioh.edu>
2025-11-05 17:56:11 -0500

    Adding Calculate function
    Implementing logic to handle all functions from a single location outside of main.

--- Simple_Calculator/Simple_Calculator/Program.cs
+++ Simple_Calculator/Simple_Calculator/Program.cs
@@ -6,10 +6,32 @@
         {
 
             // Our basic functions only perform operations on integers.
-            Console.WriteLine(Add(1, 2));
-            Console.WriteLine(Subtract(1, 2));
-            Console.WriteLine(Multiply(1, 2));
-            Console.WriteLine(Divide(1, 2));
+            Calculate(1, 2, "Add"); // expect 3
+            Calculate(4, 3, "Subtract"); //expect 1
+            Calculate(5, 3, "Multiply"); // expect 15
+            Calculate(8, 4, "Divide"); // expect 2
+        }
+
+        static void Calculate(int x, int y, string function)
+        {
+            switch (function)
+            {
+                case "Add":
+                    Console.WriteLine(Add(x, y));
+                    break;
+                case "Subtract":
+                    Console.WriteLine(Subtract(x, y));
+                    break;
+                case "Multiply":
+                    Console.WriteLine(Multiply(x, y));
+                    break;
+                case "Divide":
+                    Console.WriteLine(Divide(x, y));
+                    break;
+                default:
+                    Console.WriteLine("Function not recognized");
+                    break;
+            }
         }
 
         static int Add(int x, int y)
