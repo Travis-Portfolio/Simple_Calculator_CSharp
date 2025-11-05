4b783c1a27852fe6fb406a3e8805de7b88b7fa2b
deboartm <deboartm@miamioh.edu>
2025-11-05 17:38:50 -0500

    Including Division
    Implementing integer division functionality.

--- Simple_Calculator/Simple_Calculator/Program.cs
+++ Simple_Calculator/Simple_Calculator/Program.cs
@@ -4,15 +4,19 @@
     {
         static void Main(string[] args)
         {
+
+            // Our basic functions only perform operations on integers.
             Console.WriteLine(Add(1, 2));
             Console.WriteLine(Subtract(1, 2));
             Console.WriteLine(Multiply(1, 2));
+            Console.WriteLine(Divide(1, 2));
         }
 
         static int Add(int x, int y)
         {
             return x + y;
         }
+
         static int Subtract(int x, int y)
         {
             return x - y;
@@ -22,5 +26,11 @@
         {
             return x * y;
         }
+
+        static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
     }
 }
